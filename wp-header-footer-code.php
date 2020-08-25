fd2157f937fa88b926f58bac04d2189088983307
Nil <13785872+knilkantha@users.noreply.github.com>
2020-08-25 13:32:21 +0545

    Update wp-header-footer-code.php

--- wp-header-footer-code.php
+++ wp-header-footer-code.php
@@ -1,16 +1,14 @@
 <?php
 
 /**
- *
  * Plugin Name:       Header and Footer Code
  * Plugin URI:        https://nil.pro.np/
- * Description:       Simple plugin to add header and footer information.
+ * Description:       Simple plugin to add code on WordPress header and footer.
  * Version:           1.0.0
  * Author:            Nil
  * Author URI:        https://nil.pro.np/about/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
-
  */
 
 // If this file is called directly, abort.
@@ -18,18 +16,17 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; 
 }
 
+
+
+
+
+
+
+//Preventing accidental deactivation of plugin.
+//disabling deactivate option for this particular plugin, you can remove this section if you don't need it.
+// to deactivate this plugin comment this below four line first then wordpress will show deactive, delete option in WordPress admin.
 add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'disable_plugin_deactivation' );
 function disable_plugin_deactivation ($actions) {
 	unset( $actions['deactivate'] );
     return $actions;
 }
-
-
-
-
-// add_filter( 'plugin_action_links', 'disable_plugin_deactivation', 10, 2 );
-// function disable_plugin_deactivation( $actions, $plugin_file) { 	
-	// if ( plugin_basename( __FILE__ ) === $plugin_file ) {
-		// unset( $actions['deactivate'] );
-	// }
-    // return $actions;}
